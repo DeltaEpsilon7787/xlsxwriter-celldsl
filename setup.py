c75267a30037dbd4c71dafdf16f061f3e51a6531
Delta <editorrus2@gmail.com>
2021-05-01 03:18:59 +0500

    Bump version number

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup
 
-setup(
-    name='xlsxwriter-celldsl',
-    version='0.0.1',
-    packages=['xlsxwriter_celldsl'],
-    url='https://github.com/DeltaEpsilon7787/xlsxwriter-celldsl',
-    license='MIT',
-    author='DeltaEpsilon7787',
-    author_email='deltaepsilon7787@gmail.com',
-    description='A library to simplify writing procedular generation of Excel files using XlsxWriter and take '
-                'advantage of constant_memory mode easily.',
-    install_requires=[
-        "attrs",
-        "pytest-mock",
-        "xlsxwriter",
-    ]
-)
+with open("./README.md", encoding='utf-8') as in_:
+    setup(
+        name='xlsxwriter-celldsl',
+        version='0.1.0',
+        packages=['xlsxwriter_celldsl'],
+        url='https://github.com/DeltaEpsilon7787/xlsxwriter-celldsl',
+        license='MIT',
+        author='DeltaEpsilon7787',
+        author_email='deltaepsilon7787@gmail.com',
+        description='A library to simplify writing procedular generation of Excel files using XlsxWriter and take '
+                    'advantage of constant_memory mode easily.',
+        description_content_type="text/plain",
+        long_description=in_.read(),
+        long_description_content_type="text/markdown",
+        install_requires=[
+            "attrs",
+            "pytest-mock",
+            "xlsxwriter",
+        ]
+    )
